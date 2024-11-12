884b87803171b8fb469de41b9215b417b21551d2
Mi3van <33van3@gmail.com>
2024-11-12 14:29:51 +0300

    DEV Upd img title quality.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4041175534"
+    serviceWorkerVersion: "2989020400"
   }
 });
