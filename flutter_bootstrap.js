701604de75d0be46ebfecab5b20e5c2b7d08b041
Mi3van <33van3@gmail.com>
2024-11-12 15:16:23 +0300

    DEV Return aspectRatio to img title.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "876388786"
+    serviceWorkerVersion: "3085513830"
   }
 });
